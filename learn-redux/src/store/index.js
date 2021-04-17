e7fc3b404c73934e378ea63e30251940482a59ed
linzijun <linzjk@yonyou.com>
2021-04-17 18:59:28 +0800

    redux combineReducers实现原理

--- learn-redux/src/store/index.js
+++ learn-redux/src/store/index.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer, {
+  counter1: 10,
+  counter2: 20
+});
 
 export default store;
