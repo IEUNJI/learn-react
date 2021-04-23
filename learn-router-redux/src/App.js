6bcda49f4926bef35269f35ba4fa771f5c16d867
linzijun <linzjk@yonyou.com>
2021-04-23 15:20:25 +0800

    router-redux项目搭建

--- learn-router-redux/src/App.js
+++ learn-router-redux/src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Counter from './pages/Counter';
+
 function App() {
 
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/counter">Counter</Link>
+      </div>
+
+      <div className="view">
+        <Route path="/" exact component={Home} />
+        <Route path="/counter" component={Counter} />
+      </div>
+    </Router>
   );
 }
 
---
+++ learn-router-redux/src/pages/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import actionCreators from '../store/actionCreators/counter';
+
+class Counter extends React.Component {
+
+  render() {
+    const { number, increment, decrement } = this.props;
+
+    return (
+      <div className="counter">
+        <div>{number}</div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return state.counter;
+};
+const mapDispatchToProps = actionCreators;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
