42f3cead8f16abe92ef02285e3453ccdc07ac24d
linzijun <linzjk@yonyou.com>
2021-04-24 12:26:30 +0800

    redux浏览器插件使用

--- learn-router-redux/src/connected-react-router/index.js
+++ learn-router-redux/src/connected-react-router/index.js
@@ -3,11 +3,11 @@ import connectRouter from './connectRouter';
 import push from './push';
 import ConnectedRouter from './ConnectedRouter';
 
-// export * from 'connected-react-router';
+export * from 'connected-react-router';
 
-export {
-  routerMiddleware,
-  connectRouter,
-  push,
-  ConnectedRouter
-};
+// export {
+//   routerMiddleware,
+//   connectRouter,
+//   push,
+//   ConnectedRouter
+// };
--- learn-router-redux/src/store/index.js
+++ learn-router-redux/src/store/index.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from '../connected-react-router';
 
 import createReducer from './reducers';
 import history from './history';
 
-const store = createStore(createReducer(history), applyMiddleware(routerMiddleware(history)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(createReducer(history), composeEnhancers(applyMiddleware(routerMiddleware(history))));
 
 window.store = store;
 
